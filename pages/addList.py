fbd33b49c74ade0b09c989aee9e4ea3c03681919
Carlos Silva <silvaugusto222@gmail.com>
2024-04-09 15:31:07 -0300

    update: addList Finish

--- pages/addList.py
+++ pages/addList.py
@@ -1,22 +1,31 @@
 import streamlit as st
+from datetime import datetime
 
-# Função de limpar o input
-def clearInput():
-    st.session_state.my_text = st.session_state.stateInitInput
-    st.session_state.stateInitInput = ""
-st.write(st.session_state)
+st.title('Studying session state in Input')
+
+# Function to append data and clear input
+def handle_input(text):
+    st.session_state['text_input'] = text
+    if text:
+        st.session_state['df'].append({'content': text, 'hour': datetime.now(
+        ).strftime("%H:%M:%S"), 'date': datetime.now().strftime("%d/%m/%Y")})
+        st.session_state['text_input'] = ''  # Clear input after append
+
+# Session state for the text input
+if 'text_input' not in st.session_state:
+    st.session_state['text_input'] = ''
+
 # Criar o session_state chamado df caso ele nao exista
 if 'df' not in st.session_state:
     st.session_state['df'] = []
 
+# Observador para quando o texto mudar
+def handle_text_change():
+    text = st.session_state['text_input']  # Access current input value
+    handle_input(text)  # Call the function with the current text
+
 # Crio um input para interagir e testar
-data = st.text_input(label='Teste', key='stateInitInput', on_change=clearInput)
-st.write(st.session_state.get('my_text'))
-# Faço o append dentro do session_state df
-if data:
-    st.session_state['df'].append()
-    
-st.write(st.session_state.get('df'))
-# Apresento o session_state chamado df
-for c in st.session_state['df']:
-    st.write(c)
+st.text_input(label='', placeholder='Write Here..', key='text_input',
+              on_change=handle_text_change)
+
+st.write(st.session_state)
